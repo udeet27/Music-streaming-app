60e4b2483d217079587417408dd3bc68cb4207aa
Udeet Mittal <udeet1575@gmail.com>
2024-04-14 17:03:40 +0530

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -45,7 +45,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 
 SECURE_REFERRER_POLICY = "no-referrer-when-downgrade"
 # Application definition
